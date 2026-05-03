8ead46e4d705190f4d4b500b5aabc842cf858e1f
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2026-05-03 12:03:16 -0230

    cloudflare base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import preact from "@astrojs/preact";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  adapter: cloudflare({
+    imageService: "passthrough",
+  }),
   integrations: [icon(), preact()],
   vite: {
     plugins: [tailwindcss()],
